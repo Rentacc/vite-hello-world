5afd43077772b807ecc40fa6ef332e4fa5141e89
rentacc <rtcommercialert@gmail.com>
2023-05-17 16:01:26 +0200

    add title and jumbo

---
+++ src/components/AppHero.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="jumbotron">
+      <h2>Benvenuti nella mia app con Vite!</h2>
+      <p>Questa è la mia app Hero con Vite.</p>
+    </div>
+  </template>
+  
+  <style scoped>
+  .jumbotron {
+    background-color: goldenrod;
+    padding: 20px;
+    border-radius: 4px;
+  }
+  </style>
+  
